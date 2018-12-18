2dbdbe83a6f02c0c86cdd4cf62fd7629ffefae74
Chris Bräucker <chris.braeucker@solarisbank.de>
2018-12-18 16:20:22 +0100

    add option to move in backup

--- lib/contentful_middleman/tools/backup.rb
+++ lib/contentful_middleman/tools/backup.rb
@@ -22,7 +22,7 @@ module ContentfulMiddleman
       end
 
 
-      def initialize(name, source)
+      def initialize(name, source, move = false)
         @name   = name
         @source = source
 
@@ -31,7 +31,11 @@ module ContentfulMiddleman
         FileUtils.rm_rf(path) if ::File.exist?(path)
 
         FileUtils.mkdir(path)
-        FileUtils.cp_r(source, path)
+        if move
+          FileUtils.mv(source, path)
+        else
+          FileUtils.cp_r(source, path)
+        end
       end
 
 
