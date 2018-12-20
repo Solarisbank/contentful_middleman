1247caab78344f03b2ba994e5a15137edcb27ec6
Chris Bräucker <chris.braeucker@solarisbank.de>
2018-12-20 09:24:23 +0100

    add sync-management adapter

---
+++ lib/contentful_middleman/sync_adapter.rb
@@ -0,0 +1,27 @@
+require 'contentful_middleman/local_data/repository'
+
+module ContentfulMiddleman
+  class SyncAdapter
+    def initialize(space_name, contentful, content_type_names)
+      @space_name         = space_name
+      @contentful         = contentful
+      @content_type_names = content_type_names
+    end
+
+    def repository
+      @repository ||= LocalData::Repository.new(@space_name)
+      LocalData::Repository.exists_for?(@space_name) && @repository.load
+      @repository
+    end
+
+    def update(&block)
+      @sync = if repository.next_sync_url
+        @contentful.client.sync(repository.next_sync_url)
+      else
+        @contentful.client.sync(initial: true)
+      end
+      @sync.each_item(&block)
+      repository.next_sync_url = @sync.next_sync_url
+    end
+  end
+end
